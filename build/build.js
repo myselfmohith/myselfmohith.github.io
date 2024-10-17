510b031cb48e6c7d9d710d0a9be39245f5fa7f96
Mohith Kumar Bezawada <76643238+myselfmohith@users.noreply.github.com>
2024-10-17 21:03:03 +0530

    initial commit

---
+++ build/build.js
@@ -0,0 +1,114 @@
+const fs = require("node:fs");
+const path = require("node:path");
+const cheerio = require("cheerio");
+const showdown = require("showdown");
+
+const CONSTANTS = {
+    HTML_TEMPLATE: fs.readFileSync("build/template.html", { encoding: "utf-8" }),
+    LATEST_POSTS_JSON: "dst/scripts/latest_data.json",
+    POSTS_JSON: "dst/scripts/data.json",
+    POSTS_DIR: "dst/posts"
+}
+
+const posts = {}, utils = {};
+
+const FUNCTIONS = {
+    slugify: function (text) {
+        return text.toLocaleLowerCase().replace(/[^a-z0-9]/ig, "-").replace(/-+/g, "-").replace(/(^-)|(-$)/g, "")
+    },
+    trimText: function (text = "", size = 60) {
+        return text.length < size ? text : (text.substring(0, size - 3) + "...");
+    },
+    /**
+     * Convert's markdown to html and creates new html file in posts folder. We use the first mentioned tag to construct metadata.
+     * 1. Title = h1 tag
+     * 2. Description = h5 tag
+     * 3. Keywords = h6 tag
+     * 
+     * We use are the header tags to build index, which will be replaced at #post-index > ul.
+     * And the markdown content will be replace at #post
+     * @param {*} markDownText 
+     * @returns 
+     */
+    parseToHtml: function (markDownText) {
+        const converter = new showdown.Converter({
+            "tables": true,
+            "simplifiedAutoLink": true,
+            "strikethrough": true,
+            "tasklists": true
+        });
+        converter.setOption("ghCompatibleHeaderId", true);
+        converter.setOption("customizedHeaderId", true);
+
+        const htmlString = converter.makeHtml(markDownText);
+        const $src = cheerio.load(htmlString);
+
+        const postHead = $src('h1');
+        const postDescription = $src('h5');
+        const postKeywords = $src('h6');
+
+        const $new = cheerio.load(CONSTANTS.HTML_TEMPLATE);
+        const postTitle = postHead.text();
+        $new("title").text(FUNCTIONS.trimText(postTitle));
+        $new("meta[name=description]").attr("content", postDescription.text());
+        $new("meta[name=keywords]").attr("content", postKeywords.text());
+
+        postDescription.remove();
+        postKeywords.remove();
+
+        const postIndexUl = $new("section#page>div#post-index>ul");
+        let isFirst = true;
+        for (const headingElement of $src("h1,h2,h3,h4,h5,h6,h4")) {
+            let currentId = $src(headingElement).attr("id");
+            if (!currentId) {
+                currentId = FUNCTIONS.slugify($src(headingElement).text())
+            }
+            $src(headingElement).attr("id", currentId);
+            if (isFirst) {
+                $new(postIndexUl).append("<li><a href='#" + currentId + "'>Top</a></li>");
+                isFirst = false;
+            } else {
+                $new(postIndexUl).append("<li><a href='#" + currentId + "'>" + FUNCTIONS.trimText($src(headingElement).text(), 50) + "</a></li>");
+            }
+        }
+
+        $new("section#page>article#post").html($src.html());
+
+        return { "filename": FUNCTIONS.slugify(FUNCTIONS.trimText(postTitle), 100), "html": $new.html(), "title": postTitle, "description": postDescription.text() };
+    }
+}
+
+for (const file of fs.readdirSync("posts")) {
+    if (file.endsWith(".md")) {
+        try {
+            const stats = fs.statSync(path.join("posts", file));
+            const md = fs.readFileSync(path.join("posts", file), { encoding: "utf-8" });
+            const object = FUNCTIONS.parseToHtml(md);
+            fs.writeFileSync(path.join(CONSTANTS.POSTS_DIR, object["filename"] + ".html"), object["html"], { encoding: "utf-8" });
+            posts[object["filename"]] = { birth: stats["birthtimeMs"], ...object, html: undefined };
+            console.log("SUCCESS: " + file + " -> " + object["filename"] + ".html");
+        } catch (error) {
+            console.log("ERROR:" + file + " -> " + error.message);
+            console.log(error);
+        }
+    }
+}
+
+
+for (const file of fs.readdirSync("utils")) {
+    if (file.endsWith(".html")) {
+        try {
+            const stats = fs.statSync(path.join("utils", file));
+            utils[file] = stats["birthtimeMs"];
+        } catch (error) {
+            console.log("ERROR:" + file + " -> " + error.message);
+            console.log(error);
+        }
+    }
+}
+
+const latest_posts = Object.entries(posts).sort((a, b) => b[1]["birth"] - a[1]["birth"]).map(a => a[1]).slice(0, 5);
+const latest_utils = Object.entries(utils).sort((a, b) => b[1] - a[1]).slice(0, 5);
+
+fs.writeFileSync(CONSTANTS.LATEST_POSTS_JSON, JSON.stringify({ latest_posts, latest_utils }), { encoding: "utf-8" });
+fs.writeFileSync(CONSTANTS.POSTS_JSON, JSON.stringify({ posts, utils }), { encoding: "utf-8" });
---
+++ build/build.sh
@@ -0,0 +1,32 @@
+start_time=$(date +%s)
+
+mkdir -p dst;
+mkdir -p dst/posts dst/css dst/utils dst/scripts;
+
+echo;
+cp -rv assets dst/;
+echo "[1] Copied Assets";
+
+echo;
+cp -rv css/*.css dst/css/;
+echo "[2] Copied CSS";
+
+echo;
+cp -rv utils/*.html dst/utils/;
+echo "[3] Copied Utils";
+
+echo;
+cp -rv scripts/*.js dst/scripts/;
+echo "[4] Copied Scripts";
+
+echo;
+node build/build.js
+echo "[6] Posts Conversion Completed";
+
+echo;
+cp -v index.html dst/
+cp -v assets/icons/favicon.ico dst/
+echo "[7] Copied index.html";
+
+echo;
+echo "SUCCESS\nTotal Time: $( expr "$(date +%s)" - $start_time )s";
