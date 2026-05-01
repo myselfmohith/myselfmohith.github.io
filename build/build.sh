27c5771f3bad78265afac35cebd779cfe16ffe1e
Mohith Kumar Bezawada <bezawadamohith14@gmail.com>
2026-05-01 21:52:03 +0530

    resume first attempt

--- build/build.sh
+++ build/build.sh
@@ -24,11 +24,16 @@ node build/build.js
 echo "[6] Posts Conversion Completed";
 
 echo;
+cp -v routes/*.html dst/
+echo "[7] Copied routes";
+
+echo;
 cp -v index.html dst/
 cp -v posts.html dst/
 cp -v utils.html dst/
 cp -v assets/icons/favicon.ico dst/
-echo "[7] Copied Root Page Items";
+echo "[8] Copied Root Page Items";
 
 echo;
-echo "SUCCESS\nTotal Time: $( expr "$(date +%s)" - $start_time )s";
+echo "SUCCESS";
+echo "Total Time: $( expr "$(date +%s)" - $start_time )s";
